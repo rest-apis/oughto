bdf70e0c2d9693afde687eb12904c4993036b2db
lfborjas <luis.borjas@escolarea.com>
2011-12-03 01:42:02 -0600

    RESTful urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('tasks.views', 
         url('^$',                      '_collection', name = "tasks"),
-        url('^new$',                   'new'        , name = 'new_task'),
-        url('^(?P<task_id>\d+)/edit$', 'edit'       , name = "edit_task"),
         url('^(?P<task_id>\d+)$',      '_member'    , name = "task")
 )
