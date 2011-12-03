1389ca682afc81cc7f509844dde48e1b9850c2f0
lfborjas <luis.borjas@escolarea.com>
2011-12-03 10:26:32 -0600

    creo que es middleware para rest

--- middleware.py
+++ middleware.py
@@ -2,3 +2,16 @@ class HttpMethodsMiddleware(object):
     def process_request(self, request):
         if request.method == 'POST' and ('_method' in request.POST):
             request.method = request.POST['_method'].upper()
+
+
+from cgi import parse_qs
+import json
+
+class RestMiddleware(object):
+    def process_request(self, request):
+        if request.method == 'PUT':
+            request.REQUEST = parse_qs(request.raw_form_data)
+        if request.META['CONTENT_TYPE'] == 'application/json':
+            request.REQUEST = json.loads(request.raw_form_data)
+
+
