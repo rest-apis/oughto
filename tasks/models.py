9bf638556499f6040188b0368303a36b19e757b0
lfborjas <luis.borjas@escolarea.com>
2011-12-03 01:38:02 -0600

    Paso 2: persistencia en la base de datos

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    content = models.CharField(max_length = 255)
+    complete = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('task', [self.pk])
