c541729f2c4c47e8bf143e3a6170ec706b927c0f
lfborjas <luis.borjas@escolarea.com>
2011-12-09 17:34:18 -0600

    creando tareas

--- tasks/static/javascripts/tasks.js
+++ tasks/static/javascripts/tasks.js
@@ -7,9 +7,44 @@ Tasks = {
             });
         })
     }
+    ,complete: function(id, checked){
+        $.ajax({
+             url: '/tasks/'+id
+            ,data: {complete: checked}
+            ,type: "PUT"
+            ,dataType: "json"
+        });
+    }
+    ,create: function(content){
+        var task_template = _.template($("#task-template").html());
+        $.post('/tasks/', {content: content}, function(task){
+            $("#task-list").prepend("<li>"+task_template(task)+"</li>")
+        });
+    }
+    ,delete: function(id){
+        $.ajax({
+             url: '/tasks/'+id
+            ,type: "DELETE"
+            ,success: function(){
+                $("#task_"+id).remove();
+            }
+        });
+    }
+    
 };
 
 
 $(function(){
     Tasks.load();
+
+    //evento para cuando se cree una tarea:
+    $("#task-input").keypress(function(e){
+        var code = e.keyCode || e.which;
+        var value;
+        if (code == 13){
+           value = $(this).val();
+           $(this).val("");
+           Tasks.create(value);
+        }
+    });
 });
